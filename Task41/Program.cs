ded55a67f4205e6efe8a83c3542fe2213b805c79
Yana <jana-ivanova@mail.ru>
2022-08-25 23:47:09 +0400

    Домашняя задача 41

---
+++ Task41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Домашняя задача 41:
+// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите размерность массива:");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[sizeArray];
+Console.WriteLine("Для поиска количества положительных чисел введите их с клавиатуры:");
+for (int i = 0; i < sizeArray; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int positiveNumber = 0;
+for (int j = 0; j < sizeArray; j++)
+{
+    if (array[j] > 0)
+        positiveNumber += 1;
+}
+Console.WriteLine(
+    $"Массив [ {String.Join("; ", array)} ] содержит {positiveNumber} положительных числа"
+);
